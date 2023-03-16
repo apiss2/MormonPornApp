155615b93292863ab6ad9baff4e32abb8aedb492
apiss2 <apiss2ex@gmail.com>
2023-03-16 12:06:24 +0900

    隠す方法を複数用意

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import streamlit as st
+import cv2
 from PIL import Image
 from utils import MaskMaker, PolkaDotMaker
 
@@ -34,9 +35,15 @@ def resize_image(image: Image.Image, max_size):
     return resized_image
 
 with st.sidebar:
-    th = st.slider('Threshold value', 0.3, 0.95, 0.75)
-    min_r = st.slider('min_r', 5, 50, 30)
-    seed = st.slider('seed', 0, 2048, 0)
+    st.header('各種パラメータ')
+    th = st.slider('身体範囲推定のしきい値', 0.3, 0.95, 0.75)
+    min_r = st.slider('水玉の最小半径', 5, 50, 15)
+    seed = st.slider('乱数シード', 0, 2048, 0)
+    hide_method = st.radio('隠し方', ['固定色', '固定肌色+すりガラス', 'inpaint+すりガラス'])
+    st.text('固定色')
+    color_R = st.slider('R', 0, 255, 188)
+    color_G = st.slider('G', 0, 255, 226)
+    color_B = st.slider('B', 0, 255, 232)
 
 st.title('水玉コラ生成クソアプリ')
 
@@ -61,9 +68,24 @@ with col1:
 
 if run_button:
     if img_array is not None:
-        masks = maker.run(img_array, th)
-        dot_mask = PolkaDotMaker(seed).run(*masks, min_r=min_r)
-        image = PolkaDotMaker.chroma_key(img_array.copy(), dot_mask, (128, 255, 0))
-        col2.image(image, use_column_width=None)
+        try:
+            masks = maker.run(img_array, th)
+            dot_mask = PolkaDotMaker(seed).run(*masks, min_r=min_r)
+            if hide_method == '固定色':
+                image = PolkaDotMaker.chroma_key(img_array, dot_mask, (color_R, color_G, color_B))
+            elif hide_method == '固定肌色+すりガラス':
+                dot_mask = np.repeat(np.expand_dims(dot_mask, -1), 3, -1)
+                skin_color = img_array[masks[-1]==1].mean(axis=0).astype('uint8')
+                blur = PolkaDotMaker.chroma_key(img_array, masks[1], skin_color)
+                blur = cv2.GaussianBlur(blur, (51, 51), 70)
+                image = np.where(dot_mask==1, blur, img_array)
+            elif hide_method == 'inpaint+すりガラス':
+                dot_mask = np.repeat(np.expand_dims(dot_mask, -1), 3, -1)
+                blur = cv2.inpaint(img_array, masks[1], 3, cv2.INPAINT_TELEA)
+                blur = cv2.GaussianBlur(blur, (51, 51), 70)
+                image = np.where(dot_mask==1, blur, img_array)
+            col2.image(image, use_column_width=None)
+        except Exception as e:
+            st.error(f'不明なエラーが発生しました: {e}')
     else:
         st.error('画像を入力してください')
